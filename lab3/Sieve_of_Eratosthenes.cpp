025c7b1266ae8cce5e1df97cc804882af47898ac
uayura <48629355+uayura@users.noreply.github.com>
2019-04-15 19:17:14 +0300

    Update Sieve_of_Eratosthenes.cpp

--- lab3/Sieve_of_Eratosthenes.cpp
+++ lab3/Sieve_of_Eratosthenes.cpp
@@ -1,22 +1,25 @@
 //https://prog-cpp.ru/eratosfen/
 #include <iostream>
+#include <fstream>
 using namespace std;
 int main()
 {
 	long long n;
-	cout << "n= ";
-	cin >> n;
+	n = 2000000;
 	long long *a = new long long[n + 1];
 	for (long long i = 0; i < n + 1; i++)
 		a[i] = i;
+	ofstream fout;
+	fout.open("file.txt");
 	for (long long p = 2; p < n + 1; p++)
 	{
 		if (a[p] != 0)
 		{
-			cout << a[p] << endl;
+			fout << a[p] << endl;
 			for (long long j = p * p; j < n + 1; j += p)
 				a[j] = 0;
 		}
 	}
-	cin.get(); cin.get();
+	fout.close();
 }
+
